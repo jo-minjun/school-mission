3dc5f9150fe24706511e295725356fef171b677b
JunLife <alswns9288@gmail.com>
2022-05-26 21:01:59 +0900

    [ADD] project settings

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # school-mission
+
 학생 성적 관리 시스템 구축 미션
---
+++ src/main/java/jun/schoolmission/SchoolMissionApplication.java
@@ -0,0 +1,13 @@
+package jun.schoolmission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolMissionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolMissionApplication.class, args);
+    }
+
+}
