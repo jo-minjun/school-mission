dbf3e7973c1cf29a7e15a15c875350352a677084
JunLife <alswns9288@gmail.com>
2022-05-29 07:21:36 +0900

    [ADD] Student 조회 API 추가

--- src/main/java/jun/schoolmission/controller/StudentController.java
+++ src/main/java/jun/schoolmission/controller/StudentController.java
@@ -1,18 +1,18 @@
 package jun.schoolmission.controller;
 
 import jun.schoolmission.common.ResponseMessage;
+import jun.schoolmission.domain.dto.StudentDto;
 import jun.schoolmission.domain.dto.StudentInputDto;
+import jun.schoolmission.domain.dto.StudentOutputDto;
 import jun.schoolmission.service.StudentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/students", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -25,5 +25,14 @@ public class StudentController {
     public ResponseEntity<ResponseMessage<?>> createStudent(@RequestBody @Valid StudentInputDto studentInputDto) {
         Long id = studentService.createStudent(studentInputDto.getStudent());
         return ResponseEntity.created(URI.create("/students/" + id)).body(new ResponseMessage<>().success(null));
+    }
+
+    @GetMapping
+    public ResponseEntity<ResponseMessage<?>> searchStudents() {
+        List<StudentDto> studentDtos = studentService.searchStudentDtos();
+        StudentOutputDto studentOutputDto = StudentOutputDto.builder()
+                .students(studentDtos)
+                .build();
+        return ResponseEntity.ok().body(new ResponseMessage<>().success(studentOutputDto));
     }
 }
