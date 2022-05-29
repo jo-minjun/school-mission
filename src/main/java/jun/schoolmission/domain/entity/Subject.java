ddddb62862631602e003387fdd8ba8ef6ec09a06
JunLife <alswns9288@gmail.com>
2022-05-29 18:20:49 +0900

    [UPDATE] Subject Entity 제약 조건 추가

--- src/main/java/jun/schoolmission/domain/entity/Subject.java
+++ src/main/java/jun/schoolmission/domain/entity/Subject.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Table(uniqueConstraints = @UniqueConstraint(columnNames = {"name"}, name = "UNIQUE_NAME"))
 @Getter
 @Builder
 @AllArgsConstructor
