d05d784a3a04d4b1f222be89f8a6fadb779de2dd
JunLife <alswns9288@gmail.com>
2022-05-30 17:35:57 +0900

    [ADD] Subject 추가 서비스 로직 작성

--- src/main/java/jun/schoolmission/domain/repository/SubjectRepository.java
+++ src/main/java/jun/schoolmission/domain/repository/SubjectRepository.java
@@ -3,5 +3,8 @@ package jun.schoolmission.domain.repository;
 import jun.schoolmission.domain.entity.Subject;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface SubjectRepository extends JpaRepository<Subject, Long> {
+    Optional<Subject> findByName(String name);
 }
--- src/main/java/jun/schoolmission/service/impl/SubjectServiceImpl.java
+++ src/main/java/jun/schoolmission/service/impl/SubjectServiceImpl.java
@@ -1,12 +1,42 @@
 package jun.schoolmission.service.impl;
 
+import jun.schoolmission.common.exception.AlreadyExistException;
+import jun.schoolmission.common.exception.CustomExceptionEntity;
+import jun.schoolmission.common.exception.ErrorCode;
 import jun.schoolmission.domain.dto.subject.SubjectDto;
+import jun.schoolmission.domain.entity.Student;
+import jun.schoolmission.domain.entity.Subject;
+import jun.schoolmission.domain.repository.StudentRepository;
+import jun.schoolmission.domain.repository.SubjectRepository;
 import jun.schoolmission.service.SubjectService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class SubjectServiceImpl implements SubjectService {
 
+    private final SubjectRepository subjectRepository;
+    private final StudentRepository studentRepository;
+
     @Override
+    @Transactional
     public Long createSubject(SubjectDto subjectDto) {
-        return null;
+        subjectRepository.findByName(subjectDto.getName()).ifPresent(subject -> {
+            throw new AlreadyExistException(CustomExceptionEntity.builder()
+                    .errorCode(ErrorCode.ALREADY_EXIST_SUBJECT)
+                    .explain(subject.getName())
+                    .build());
+        });
+
+        Subject subject = subjectDto.toEntity();
+
+        List<Student> students = studentRepository.findAll();
+        subject.registerStudents(students);
+
+        return subjectRepository.save(subject).getId();
     }
 }
