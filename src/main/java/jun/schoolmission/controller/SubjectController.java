627b8e5253c6c06d475bc7df27f764f5dfd8ad02
JunLife <alswns9288@gmail.com>
2022-05-31 22:20:09 +0900

    [ADD] Subject 조회 컨트롤러 개발

--- src/main/java/jun/schoolmission/controller/SubjectController.java
+++ src/main/java/jun/schoolmission/controller/SubjectController.java
@@ -1,18 +1,18 @@
 package jun.schoolmission.controller;
 
 import jun.schoolmission.common.ResponseMessage;
+import jun.schoolmission.domain.dto.subject.SubjectDto;
 import jun.schoolmission.domain.dto.subject.SubjectInputDto;
+import jun.schoolmission.domain.dto.subject.SubjectOutputDto;
 import jun.schoolmission.service.SubjectService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,6 +25,15 @@ public class SubjectController {
     public ResponseEntity<ResponseMessage<?>> createSubject(@RequestBody @Valid SubjectInputDto subjectInputDto) {
         Long id = subjectService.createSubject(subjectInputDto.getSubject());
         return ResponseEntity.created(URI.create("/subjects/" + id)).body(new ResponseMessage<>().success(null));
+    }
+
+    @GetMapping
+    public ResponseEntity<ResponseMessage<?>> searchSubjects() {
+        List<SubjectDto> subjectDtos = subjectService.searchSubjectDtos();
+        SubjectOutputDto subjectOutputDto = SubjectOutputDto.builder()
+                .subjects(subjectDtos)
+                .build();
+        return ResponseEntity.ok().body(new ResponseMessage<>().success(subjectOutputDto));
     }
 }
 
