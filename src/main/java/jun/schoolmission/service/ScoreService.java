b474ecbd41e2234f92ec3ec01d9c5cb79ca8125e
JunLife <alswns9288@gmail.com>
2022-06-02 17:07:28 +0900

    [ADD] StudentSubject Service 점수 업데이트 개발

--- src/main/java/jun/schoolmission/service/ScoreService.java
+++ src/main/java/jun/schoolmission/service/ScoreService.java
@@ -4,6 +4,6 @@ import jun.schoolmission.domain.dto.score.ScoreDto;
 
 public interface ScoreService {
 
-    Long updateScore(Long studentId, Long subjectId, ScoreDto scoreDto);
+    void updateScore(Long studentId, Long subjectId, ScoreDto scoreDto);
     void deleteScore(Long studentId, Long subjectId);
 }
--- src/main/java/jun/schoolmission/service/impl/ScoreServiceImpl.java
+++ src/main/java/jun/schoolmission/service/impl/ScoreServiceImpl.java
@@ -1,16 +1,57 @@
 package jun.schoolmission.service.impl;
 
+import jun.schoolmission.common.exception.CustomExceptionEntity;
+import jun.schoolmission.common.exception.ErrorCode;
+import jun.schoolmission.common.exception.NotFoundException;
 import jun.schoolmission.domain.dto.score.ScoreDto;
+import jun.schoolmission.domain.entity.Student;
+import jun.schoolmission.domain.entity.StudentSubject;
+import jun.schoolmission.domain.repository.StudentRepository;
 import jun.schoolmission.service.ScoreService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
 public class ScoreServiceImpl implements ScoreService {
 
+    private final StudentRepository studentRepository;
+
     @Override
-    public Long updateScore(Long studentId, Long subjectId, ScoreDto scoreDto) {
-        return null;
+    @Transactional
+    public void updateScore(Long studentId, Long subjectId, ScoreDto scoreDto) {
+        Optional<Student> studentOptional = studentRepository.findById(studentId);
+        StudentSubject studentSubject = findStudentSubject(studentOptional, studentId, subjectId);
+
+        Student student = studentOptional.get();
+        studentSubject.updateScore(scoreDto.getScore());
+
+        studentRepository.save(student);
     }
 
     @Override
     public void deleteScore(Long studentId, Long subjectId) {
+    }
+
+    private StudentSubject findStudentSubject(Optional<Student> studentOptional, Long studentId, Long subjectId) {
+        List<StudentSubject> studentSubjects = studentOptional.orElseThrow(() ->
+                new NotFoundException(CustomExceptionEntity.builder()
+                        .errorCode(ErrorCode.STUDENT_NOT_FOUND)
+                        .explain(studentId.toString())
+                        .build())
+        ).getStudentSubjects();
+
+        return studentSubjects.stream().filter(studentSubject -> studentSubject.getSubject().getId().equals(subjectId))
+                .findFirst()
+                .orElseThrow(() ->
+                        new NotFoundException(CustomExceptionEntity.builder()
+                                .errorCode(ErrorCode.SUBJECT_NOT_FOUND)
+                                .explain(subjectId.toString())
+                                .build())
+                );
     }
 }
