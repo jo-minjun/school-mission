64657d5e4b5358240439eba2d454cec1bed00f3c
JunLife <alswns9288@gmail.com>
2022-06-03 00:11:40 +0900

    [ADD] ScoreController 할당 개발

--- src/main/java/jun/schoolmission/controller/ScoreController.java
+++ src/main/java/jun/schoolmission/controller/ScoreController.java
@@ -1,21 +1,26 @@
 package jun.schoolmission.controller;
 
 import jun.schoolmission.common.ResponseMessage;
+import jun.schoolmission.domain.dto.score.ScoreDto;
 import jun.schoolmission.service.ScoreService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.net.URI;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 public class ScoreController {
 
     private final ScoreService scoreService;
 
     @PostMapping(value = "/students/{studentId}/subjects/{subjectId}/scores")
-    public ResponseEntity<ResponseMessage<?>> createScore(@PathVariable Long studentId, @PathVariable Long subjectId) {
-        return null;
+    public ResponseEntity<ResponseMessage<?>> createScore(@PathVariable Long studentId, @PathVariable Long subjectId, @RequestBody @Valid ScoreDto scoreDto) {
+        scoreService.updateScore(studentId, subjectId, scoreDto);
+        return ResponseEntity.created(URI.create("")).body(new ResponseMessage<>().success(null));
     }
 }
--- src/main/java/jun/schoolmission/domain/dto/score/ScoreDto.java
+++ src/main/java/jun/schoolmission/domain/dto/score/ScoreDto.java
@@ -3,12 +3,17 @@ package jun.schoolmission.domain.dto.score;
 import jun.schoolmission.domain.entity.Score;
 import lombok.*;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+
 @Getter
 @Builder
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 @AllArgsConstructor
 public class ScoreDto {
 
+    @Min(value = 0, message = "score는 0 ~ 100만 가능합니다.")
+    @Max(value = 100, message = "score는 0 ~ 100만 가능합니다.")
     private Integer score;
 
     public static ScoreDto of(Score score) {
