02f294a2e9f3c243df934eacc3e4439959967675
JunLife <alswns9288@gmail.com>
2022-06-06 22:52:00 +0900

    [ADD] StudentScoreDto 개발

--- src/main/java/jun/schoolmission/domain/dto/score/StudentScoreDto.java
+++ src/main/java/jun/schoolmission/domain/dto/score/StudentScoreDto.java
@@ -1,2 +1,33 @@
-package jun.schoolmission.domain.dto.score;public class StudentScoreDto {
+package jun.schoolmission.domain.dto.score;
+
+import jun.schoolmission.domain.dto.subject.SubjectDto;
+import jun.schoolmission.domain.entity.StudentSubject;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor
+public class StudentScoreDto {
+
+    private Double averageScore;
+    private List<SubjectDto> subjects;
+
+    public static StudentScoreDto of(List<StudentSubject> studentSubjects) {
+        List<SubjectDto> subjectDtos = new ArrayList<>();
+        int sum = 0;
+
+        for (StudentSubject studentSubject : studentSubjects) {
+            subjectDtos.add(SubjectDto.of(studentSubject.getSubject()));
+            sum += studentSubject.getScore().getScore();
+        }
+
+        return StudentScoreDto.builder()
+                .averageScore(sum / (double) studentSubjects.size())
+                .subjects(subjectDtos)
+                .build();
+    }
 }
